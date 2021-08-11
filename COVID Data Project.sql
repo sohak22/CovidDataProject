d5f7726ecb0ca4cca0b7620a4b08b6df7e7e01c7
sohak22 <76765744+sohak22@users.noreply.github.com>
2021-08-11 19:02:39 -0400

    Update COVID Data Project.sql

--- COVID Data Project.sql
+++ COVID Data Project.sql
@@ -130,3 +130,45 @@ Where death.continent is not null
 
 Select * 
 From PercentPopulationVaccinated
+
+
+/*
+Queries used for Tableau Project
+*/
+
+--1
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From portfolioProject..covid_Deaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From portfolioProject..covid_Deaths$
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From portfolioProject..covid_Deaths$
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From portfolioProject..covid_Deaths$
+Group by Location, Population, date
+order by PercentPopulationInfected desc
